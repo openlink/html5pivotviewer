337ffd30d69fe48851781778da5c9d24b3ff2308
Patrick van Kleef <pkleef@openlinksw.com>
2014-01-20 14:13:28 +0100

    Updated copyright year

--- vsp/skin/ss/style.css
+++ vsp/skin/ss/style.css
@@ -1,7 +1,7 @@
 /*
  *   This file is part of the html5 pivotviewer project
  *
- *   Copyright (C) 2012-2013 OpenLink Software
+ *   Copyright (C) 2012-2014 OpenLink Software
  *
  *   This project is free software; you can redistribute it and/or modify it
  *   under the terms of the GNU General Public License as published by the
